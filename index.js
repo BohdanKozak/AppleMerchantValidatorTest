eb2ac05f5392338b3722274d1a79e0c199eed945
bohdan.kozak <bohdan.kozak@everymatrix.com>
2025-07-18 13:13:40 +0300

    FIX

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const fs = require('fs');
 const path = require('path');
 const https = require('https');
-const fetch = require('node-fetch');
+const fetch = (...args) => import('node-fetch').then(({ default: fetch }) => fetch(...args));
 
 const app = express();
 app.use(express.json());
